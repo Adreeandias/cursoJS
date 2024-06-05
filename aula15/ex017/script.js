13ca0b27069665b303683dd018520ef8c94f2889
Adreeandias <adreeandias1@gmail.com>
2024-06-05 19:46:52 -0300

    exercicio concluido

--- aula15/ex017/script.js
+++ aula15/ex017/script.js
@@ -1,21 +1,37 @@
 function contar() {
-    var inicio = document.getElementById('icon')
-    var fim = document.getElementById('fcon')
-    var passo = document.getElementById('pass')
+    var ic = document.getElementById('icon')
+    var fc = document.getElementById('fcon')
+    var pc = document.getElementById('pass')
     var res = document.getElementById('res')
+    var i = Number(ic.value)
+    var f = Number(fc.value)
+    var p = Number(pc.value)
+    
 
-    var i = (inicio.value)
-    var f = (fim.value)
-    var p = (passo.value)
-
-    if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
+    if (ic.value.length == 0 || fc.value.length == 0 || pc.value.length == 0) {
         window.alert('ERROR Faltam Dados!!')
+        res.innerHTML = 'Impossivel contar!'
     } else {
-        res.innerHTML = 'contando: '
-
-        for (var c = i; c <= f; c+= p) {
-            
+        res.innerHTML = 'contando: <br>'
+        if (p<=0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p=1
         }
-    } 
 
+        if (i<f) {
+            //crescente
+            for(var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F334}`
+            }
+
+
+        } else {
+        //regrassiva
+            for(var c = i; c >= f; c-= p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
